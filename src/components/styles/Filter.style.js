a11899a3bcdb0a1a2fc0a992c3cad8f7175ef09f
avag <avagsargsyan01@gmail.com>
2022-11-20 21:09:21 +0400

    Style Filter component

--- src/components/styles/Filter.style.js
+++ src/components/styles/Filter.style.js
@@ -1,5 +1,16 @@
 import styled from 'styled-components';
 
 export const StyledFilter = styled.div`
-  
+  width: 15%;
+  min-width: 12rem;
+
+  select {
+    width: 100%;
+    padding: 1rem 1.5rem;
+    background: ${({ theme }) => theme.componentBg};
+    border: none;
+    box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+    border-radius: 5px;
+    outline: none !important;
+  }
 `
--- src/components/styles/SearchCountries.style.js
+++ src/components/styles/SearchCountries.style.js
@@ -3,10 +3,11 @@ import styled from 'styled-components';
 export const StyledSearchCountries = styled.div`
   background: ${({ theme }) => theme.background};
   color: ${({ theme }) => theme.textColor};
-  padding: 2% 5%;
+  padding: 2% 6%;
   display: flex;
   flex-wrap: wrap;
   align-items: center;
   justify-content: space-between;
   gap: 0.2rem;
+  row-gap: 2rem;
 `
